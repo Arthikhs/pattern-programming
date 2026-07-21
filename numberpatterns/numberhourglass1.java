5ba0087daf4f1abbf5c886b2ccce9eafffa20b35
Arthik H S <hsarthik5@gmail.com>
2026-07-21 23:43:55 +0530

    Add 12 new patterns and update README

--- numberpatterns/numberhourglass1.java
+++ numberpatterns/numberhourglass1.java
@@ -1,6 +1,7 @@
+
 package numberpatterns;
 import java.util.Scanner; 
-public class numberhourglass {
+public class numberhourglass1 {
     public static void main(String[]args){
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
@@ -9,17 +10,18 @@ public class numberhourglass {
                 System.out.print("  ");
             }
             for(int k= 0;k<=2*i+1;k++){
-                System.out.print("* ");
+                System.out.print(i + " ");
             }
             System.out.println();
 
         }
-        for(int i = 0;i<n;i++){
+        for(int i = 0 ;i<=n-1;i++){
             for(int j= 0;j<n-i-1;j++){
                 System.out.print("  ");
+
             }
             for(int k= 0;k<=2*i+1;k++){
-                System.out.print("* ");
+                System.out.print(i + " ");
             }
             System.out.println();
 
